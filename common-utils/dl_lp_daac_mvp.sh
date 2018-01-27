68f5eafc0b784fd0a37e200b477fb39bd164bbb3
Zhan Li <zhanli1986@gmail.com>
2018-01-26 23:20:47 -0500

    Fix script to download data from LP DAAC

--- common-utils/dl_lp_daac_mvp.sh
+++ common-utils/dl_lp_daac_mvp.sh
@@ -1,98 +1,149 @@
 #!/bin/bash
 
-url_base1=https://e4ftl01.cr.usgs.gov/MODV6_Cmp_C/MOTA
-url_base2=https://e4ftl01.cr.usgs.gov/MODV6_Cmp_B/MOTA
+usage="$0 --user [your_user_name] --password [your_password] -f ['h5'|'hdf'] -t [tile] -y [year] -p [parm] -n [num] -o <out_dir> -b <begin_doy> -e <end_doy>"
 
+url_base='https://e4ftl01.cr.usgs.gov/MOTA/'
+
+# jul year doy
+# return YYYYMMDD
 jul () { date -d "$1-01-01 +$2 days -1 day" "+%Y%m%d"; }
 
-year=$1
-band=$2
+# wget retry times
+retry=10
+MIN_SLEEPTIME=10
+MAX_SLEEPTIME=30
 
-if [ -z $year ]; then
-	echo "year=?"
+fmt=h5
+tile="*"
+# parse parameters
+while [[ $# > 1 ]]
+  do
+  key="$1"
+  shift
+
+  case $key in
+    --user)
+        user="$1"
+        shift
+        ;;
+    --password)
+        password="$1"
+        shift
+        ;;
+    -f|--format)
+        fmt="$1"
+        shift
+        ;;
+    -t|--tile)
+        tile="$1"
+        shift
+        ;;
+    -y|--year)
+        year="$1"
+        shift
+        ;;
+    -n|--num)
+        num="$1"
+        shift
+        ;;
+    -p|--par)
+        par="$1"
+        shift
+        ;;
+    -o|--output)
+        dir="$1"
+        shift
+        ;;
+    -b|--begin)
+        begin_doy="$1"
+        shift
+        ;;
+    -e|--end)
+        end_doy="$1"
+        shift
+        ;;
+    *)
+        # unknown option
+        ;;
+  esac
+done
+
+if [ -z $year ] || [ -z $num ] || [ -z ${user} ] || [ -z ${password} ]; then
+	echo $usage
+	exit
+fi
+if [ "$tile" == "all" ] || [ "$tile" == "*" ]; then
+    tile="all"
+    tilestr="";
+else
+    tilestr="${tile}."
+fi
+
+if [ -z $dir ]; then
+	dir="./"
+fi
+if [ -z $begin_doy ]; then
+	begin_doy=1
+	if [ $year -eq 2000 ]; then
+		begin_doy=55
+	fi
+fi
+if [ -z $end_doy ]; then
+    let n1=$year%4
+    let n2=$year%100
+    let n3=$year%400
+    if [ $n1 -eq 0 -a $n2 -ne 0 ] || [ $n3 -eq 0 ]; then
+        end_doy=366
+    else
+        end_doy=365
+    fi
+fi
+
+if [ ! -r $dir ]; then
+	echo "$dir not exists."
 	exit
 fi
 
+out_dir=${dir}/${year}
+if [ ! -r $out_dir ]; then
+	mkdir -p $out_dir
+fi
+
+cd $out_dir
+
+LOG="wget_lp_daac_${tile}_${year}_$(printf %03d ${begin_doy})_$(printf %03d ${end_doy}).log"
+>${LOG}
+
+numstr=`printf %03d ${num}`
+
 count=0
+for ((doy=${begin_doy}; doy<=${end_doy}; doy++));
+do
+    doystr=`printf %03d ${doy}`
 
-arr=(31 40 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21)
+    echo "Downloading ${par} ${tile} ${year}${doystr}"
 
-for ((i=0; i<23; i++)); do
-	num=${arr[$i]}
+    yyyymmdd=`jul $year $doy`
+    yyyy=${yyyymmdd:0:4}
+    mm=${yyyymmdd:4:2}
+    dd=${yyyymmdd:6:2}
+    
+    # MODIS
+    if [ $yyyymmdd -lt 20000224 ]; then
+        continue
+    fi
 
-	if [ ! -z $band ]; then
-		min=$((${band}*3-2))
-		max=$((${min}+2))
-		if [ $num -lt $min ] || [ $num -gt $max ]; then
-			continue
-		fi
-	fi
-
-	for doy in {-120..486}; do
-
-		yyyymmdd=`jul $year $doy`
-		yyyy=${yyyymmdd:0:4}
-		mm=${yyyymmdd:4:2}
-		dd=${yyyymmdd:6:2}
-	
-		#MODIS
-		if [ $yyyymmdd -lt 20000224 ]; then
-			continue
-		fi
-		
-		day=`date -d "${mm}/${dd}/${yyyy}" +%j`
-		nn=`printf %02d $num`
-
-		exist=`find ./${yyyy} -name "MCD43D${nn}.A${yyyy}${day}.006.*.hdf"`
-		if [ ! -z $exist ]; then
-#			echo "Skip $exist"
-			continue
-		fi
-	
-		str2=${yyyy}.${mm}.${dd}
-
-		str1=MCD43D${nn}.006
-
-		url=${url_base1}/${str1}/${str2}/
-		if [ $num -eq 31 ]; then
-			url=${url_base2}/${str1}/${str2}/
-		fi
-
-		wget -q -O tmp.html $url
-		if [ $? -ne 0 ]; then
-			echo "error found in downloading $url"
-			continue
-		fi
-
-		hdf=`cat tmp.html | grep ".hdf<" | awk -F ">" '{print $3}'`
-		hdf=${hdf%<*}
-		if [ "${hdf}" == "" ]; then
-			continue
-		fi
-
-		dir=./${yyyy}
-		if [ ! -r $dir ]; then
-			mkdir $dir
-		fi
-		cd $dir
-		
-		if [ ! -r $hdf ]; then
-			url=${url}${hdf}
-			stime=`date +%Y-%m-%d:%H:%M:%S`
-			printf "[${stime}]Downloading ${dir}/${hdf} "
-			SECONDS=0
-			if [ $count -eq 0 ]; then
-				wget -q --save-cookies cookies.txt --keep-session-cookies --http-user=USERNAME --http-password=PASSWORD $url
-			else
-				wget -q --load-cookies cookies.txt $url
-			fi
-			count=$((${count}+1))
-
-			size=`ls -l $hdf | awk '{print $5}'`
-			speed=`echo "${size}/1024/1024/${SECONDS}" | bc -l`
-			speed=`printf %5.2f $speed`
-			printf "[${size} BYTES, ${speed} M/S]\n"
-		fi
-		cd ..
-	done
+    fpat="${par}.A${year}${doystr}.${tilestr}${numstr}.*.${fmt}"
+    rdir="${par}.${numstr}/${year}.${mm}.${dd}/"
+    if [ $count -eq 0 ]; then
+        wget -a ${LOG} --save-cookies cookies.txt --keep-session-cookies --http-user="${user}" --http-password="${password}" --wait=15 -t 0 --waitretry=${retry} --random-wait -r -l 1 -nd --no-parent -A "${fpat}" ${url_base}${rdir}
+    else
+        wget -a ${LOG} --load-cookies cookies.txt --wait=15 -t 0 --waitretry=${retry} --random-wait -r -l 1 -nd --no-parent -A "${fpat}" ${url_base}${rdir}
+    fi
+    count=$((${count}+1))
+    
+    sleep $((RANDOM%(MAX_SLEEPTIME-MIN_SLEEPTIME)+MIN_SLEEPTIME))
 done
+
+rm -f cookies.txt
+echo "Finished downloading ${par} of ${tile} of ${year} from ${begin_doy} to ${end_doy}"
