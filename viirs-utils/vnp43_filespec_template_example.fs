5babfc8f0311acb6b1ede9016b0632be0c58b22d
Zhan Li <zhanli1986@gmail.com>
2018-02-13 00:04:54 -0500

    Add an exmple template for VNP43 filespec generation.

---
+++ viirs-utils/vnp43_filespec_template_example.fs
@@ -0,0 +1,59 @@
+=======================================================================
+
+
+[ShortName] V6.0.0 daily L3
+------------------------
+
+Suomi NPP HDF5 File Specification
+Version:        [AlgorithmVersion]
+Revision:       0
+Date:           [Date]
+ESDT:		[ShortName]
+Name:		[LongName]
+Level:		[Level]
+Process ID:	[AlgorithmIdentity]
+
+Point of Contact:
+			Joe John
+            BlaBla Lab
+            University of Wonderland, STATE, Country
+            Office 555 555 5555; Fax 555 555 5554
+            joe.john@wonder.edu
+
+Principal Investigator:
+			Joe John
+            BlaBla Lab
+            University of Wonderland, STATE, Country
+            Office 555 555 5555; Fax 555 555 5554
+            joe.john@wonder.edu
+
+=======================================================================
+
+Product Identifier: [ShortName]
+
+Description: [LongName]
+
+Contents:    
+
+SDS names:
+[DataFields]
+
+Global Metadata:
+	StructMetadata.0
+	HDFEOSVersion
+
+*Start Global Attributes ==============================================
+
+[GlobalAttributes]
+
+*End Global Attributes ================================================
+
+*Start StructMetadata.0================================================
+
+[StructMetadata.0]
+
+*End StructMetadata.0 =================================================
+
+*Begin SDS Definitions ================================================
+[DataFieldDefinitions]
+*End SDS Definitions ==================================================
